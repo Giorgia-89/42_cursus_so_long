724b93b2888ae604e747c5d61235550f05f1ad9c
Giorgia-89 <giorgiadinno@gmail.com>
2023-11-02 14:26:19 +0000

    fixed error when invalid char is present -map was being displayed before

--- src/check_map.c
+++ src/check_map.c
@@ -6,23 +6,23 @@
 /*   By: gd-innoc <gd-innoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:22:33 by gd-innoc          #+#    #+#             */
-/*   Updated: 2023/11/01 14:32:47 by gd-innoc         ###   ########.fr       */
+/*   Updated: 2023/11/02 14:24:27 by gd-innoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-int	ft_map_error(char *error)
+int ft_map_error(char *error)
 {
 	ft_printf("Error\n%s", error);
 	return (-1);
 }
 
 /*this function checks that the map is surrounded by 1s on all sides*/
-int	ft_check_borders(t_data *data)
+int ft_check_borders(t_data *data)
 {
-	int	x;
-	int	y;
+	int x;
+	int y;
 
 	y = 0;
 	while (y < (data->height / IMG_SIZE))
@@ -34,8 +34,9 @@ int	ft_check_borders(t_data *data)
 		{
 			if ((y == 0 || x == 0) && data->map.map[y][x] != '1')
 				return (ft_map_error("Map must be surrounded by walls\n"));
-			else if ((y == (data->height / IMG_SIZE - 1) || \
-			x == (data->width / IMG_SIZE - 1)) && data->map.map[y][x] != '1')
+			else if ((y == (data->height / IMG_SIZE - 1) ||
+					  x == (data->width / IMG_SIZE - 1)) &&
+					 data->map.map[y][x] != '1')
 				return (ft_map_error("Map must be surrounded by walls\n"));
 			x++;
 		}
@@ -44,7 +45,7 @@ int	ft_check_borders(t_data *data)
 	return (0);
 }
 
-int	ft_check_requisites(int collectible, int player, int exit)
+int ft_check_requisites(int collectible, int player, int exit)
 {
 	if (player != 1)
 	{
@@ -67,12 +68,12 @@ int	ft_check_requisites(int collectible, int player, int exit)
 /*this function takes as argument the data structure and checks that it
 contains only valid chars. Valid chars are 0, 1, P, E, C; P and E are
 initialised here and passed to count function to cut down lines*/
-int	ft_check_contents(t_data *data)
+int ft_check_contents(t_data *data)
 {
-	int	y;
-	int	x;
-	int	plyr;
-	int	exit;
+	int y;
+	int x;
+	int plyr;
+	int exit;
 
 	plyr = 0;
 	exit = 0;
@@ -83,10 +84,11 @@ int	ft_check_contents(t_data *data)
 		y = 1;
 		while (y < data->height / IMG_SIZE - 1)
 		{
-			if (data->map.map[y][x] != '1' && data->map.map[y][x] != '0' \
-			&& data->map.map[y][x] != 'P' && data->map.map[y][x] != 'E' \
-			&& data->map.map[y][x] != 'C')
-				return (ft_printf("Error\nMap contains invalid characters\n"));
+			if (data->map.map[y][x] != '1' && data->map.map[y][x] != '0' && data->map.map[y][x] != 'P' && data->map.map[y][x] != 'E' && data->map.map[y][x] != 'C')
+			{
+				ft_printf("Error\nMap contains invalid characters\n");
+				return (-1);
+			}
 			else if (data->map.map[y][x] == 'C')
 				data->map.collectible++;
 			y++;
